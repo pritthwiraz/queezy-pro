0134d340d656d9f415ec3de83875963d9240603e
pritthwiraz <pritthwiraz@gmail.com>
2022-10-12 20:35:11 +0600

    style added for Quiz Components

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     const quizes =  loaderData.data
     console.log(quizes)
     return (
-        <div>
+        <div className='container mx-auto mt-7 grid grid-cols-1 md:grid-cols-4 gap-3 '>
            {
             quizes.map( quiz => <Quiz
             key = {quiz.id}
--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -3,11 +3,12 @@ import React from 'react';
 const Quiz = ({quiz}) => {
     const {name, logo} = quiz
     return (
-        <div className=''>
-         <div>
-         <img src={logo} alt="" />
+        <div className='border border-black p-3 bg-zinc-100'>         
+         <img className='border border-black' src={logo} alt="" />
+        <div className='flex justify-between mt-2'>
         <p>Name: {name}</p>
-         </div>
+        <button className='bg-blue-500 p-2'>Start Quiz</button>
+        </div>
         </div>
     );
 };
